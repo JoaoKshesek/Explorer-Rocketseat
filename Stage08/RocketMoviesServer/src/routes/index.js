cd341c05d6cbca773fe976003f6b8887941d0ef3
JoaoKshesek <kshesek.joaovalter@gmail.com>
2023-07-23 11:15:51 -0300

    feat: update stage 08

--- Stage08/RocketMoviesServer/src/routes/index.js
+++ Stage08/RocketMoviesServer/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const usersRouter = require("./user.routes");
 const movieNotesRoutes = require("./movieNotes.routes");
 const movieTagsRouter = require("./movieTags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
-routes.use("/notes", movieNotesRoutes);
+routes.use("/sessions", sessionsRouter);
+routes.use("/movies", movieNotesRoutes);
 routes.use("/tags", movieTagsRouter);
 
 module.exports = routes;
